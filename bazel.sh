74e96da15a43056c37cc58469ac2d318dc2ef955
Yarden Eitan <yar@google.com>
2018-02-23 14:46:34 -0500

    wip

--- bazel.sh
+++ bazel.sh
@@ -128,6 +128,7 @@ if [ -n "$KOKORO_BUILD_NUMBER" ]; then
       xcodebuild -version
       xcrun simctl list
       xcodebuild -showsdks
+      xcrun simctl create 'iPhone 6' com.apple.CoreSimulator.SimDeviceType.iPhone-6 ${sdks[i]}
       # Resolves the following crash when switching Xcode versions:
       # "Failed to locate a valid instance of CoreSimulatorService in the bootstrap"
       launchctl remove com.apple.CoreSimulator.CoreSimulatorService || true
